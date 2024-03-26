dadcf6cdf3886bf9100d275b3a230b29781054f8
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-26 15:42:58 +0300

    task1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert a node to the left
+ * @parent: the parent node
+ * @value: value of the node
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+	binary_tree_t *ptr = malloc(sizeof(binary_tree_t));
+	if (ptr == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		ptr->left = parent->left;
+		ptr->n = value;
+		ptr->right = NULL;
+		ptr->parent = parent;
+		parent->left->parent = ptr;
+		parent->left = ptr;
+	}
+	else if (parent->left == NULL)
+	{
+		parent->left = ptr;
+		ptr->n = value;
+		ptr->right = NULL;
+		ptr->left = NULL;
+	}
+	return (ptr);
+}
