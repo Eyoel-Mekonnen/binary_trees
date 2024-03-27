6a9ca57e2fa526781fdd41d09f38f9e976530004
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-27 18:36:45 +0300

    task9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: address of the node
+ *
+ * Return: size of the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	binary_tree_height(tree->left);
+	if (tree->left != NULL)
+		left_height++;
+	binary_tree_height(tree->right);
+	if (tree->right != NULL)
+		right_height++;
+	if (right_height > left_height)
+		return (right_height);
+	else
+		return(left_height);
+
+}
