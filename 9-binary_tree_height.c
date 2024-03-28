2f31b031e70d14f3756a433455ee7e54138e5681
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-28 12:43:25 +0300

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
+	if (tree == NULL)
+		return (0);
 	if (tree->left != NULL)
 		left_height = binary_tree_height(tree->left) + 1;
 	if (tree->right != NULL)
