bcd8a81070bc96c6f8530892c3e7618e58925f85
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-03-31 13:19:05 +0300

    task15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks wether a tree is full or not
+ * @tree: address of the node
+ *
+ * Return: 1 if full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left_tree = 0;
+	int right_tree = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		left_tree = binary_tree_is_full(tree->left);
+		right_tree = binary_tree_is_full(tree->right);
+	}
+	return (left_tree && right_tree);
+}
