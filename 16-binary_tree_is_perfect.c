1d73467362aad4cd8b8520ca2094b199eebce64b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-02 11:42:37 +0300

    hot_fix_task_16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,20 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height = 0;
-        size_t right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left != NULL)
-                left_height = binary_tree_height(tree->left) + 1;
-        if (tree->right != NULL)
-                right_height = binary_tree_height(tree->right) + 1;
-        if (right_height > left_height)
-                return (right_height);
-        else
-                return (left_height);
-
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+	if (right_height > left_height)
+		return (right_height);
+	else
+		return (left_height);
 }
 /**
  * binary_tree_is_perfect - check if a tree is perfect
