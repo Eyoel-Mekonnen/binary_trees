3cbbe43457e89df2594ba72b1f3fe74842304a15
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-02 11:51:02 +0300

    task17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,11 +11,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node->parent == NULL)
 		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
 	else
-	{
-		if (node == node->parent->left)
-			return (node->parent->right);
-		else
-			return (node->parent->left);
-	}
+		return (node->parent->left);
 }
