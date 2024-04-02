5f9e0698c937a38f9a5c3eccb0350ce71e9383e4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-02 11:27:37 +0300

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		left_tree =  binary_tree_height(tree->left);
 		right_tree = binary_tree_height(tree->right);
 		if (left_tree == right_tree)
-			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+			return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
 		else
 			return (0);
 	}
