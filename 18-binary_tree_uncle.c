0829a5cf72853bb7bd4d4cb75d8d9b2c3a031eb4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-02 12:05:42 +0300

    task18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: address of the node
+ *
+ * Return: address of the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
